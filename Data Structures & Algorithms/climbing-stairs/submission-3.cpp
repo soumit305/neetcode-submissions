6b1266cd66b00ea0159df2f0dff86d71f61c3a2a
Soumit Mishra <67151018+soumit305@users.noreply.github.com>
2026-07-24 21:39:21 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,1);
+        for (int i = 2; i<=n; i++){
+            int count = 0;
+            count += dp[i-1];
+            count += dp[i-2];
+            dp[i] = count;
+        }
+        return dp[n];
+        // return solve(n,dp);
+    }
+    int solve(int n, vector<int>& dp){
+        if (n==0 || n==1) return 1;
+        if (dp[n]!=-1) return dp[n];
+        int count = 0;
+        count += solve(n-1,dp);
+        count += solve(n-2,dp);
+        return dp[n] = count;
+    }
+};
