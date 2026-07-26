37e96cba5806e352b27ad794e38af1a8cc3bc1be
Soumit Mishra <67151018+soumit305@users.noreply.github.com>
2026-07-26 18:39:07 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,39 @@
+class DisjointSet{
+public:
+    vector<int> parent,size;
+    DisjointSet(int n){
+        size.resize(n,1);
+        parent.resize(n);
+        for (int i = 0; i<n; i++) parent[i] = i;
+    }
+    int findUPar(int node){
+        if (node == parent[node]) return node;
+        else return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v){
+        int ultu = findUPar(u);
+        int ultv = findUPar(v);
+        if (size[ultu]>size[ultv]){
+            parent[ultv] = ultu;
+            size[ultu] += size[ultv];
+        } else {
+            parent[ultu] = ultv;
+            size[ultv] += size[ultu]; 
+        }
+    }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DisjointSet ds(n);
+        for (auto it : edges){
+            ds.unionBySize(it[0],it[1]);
+        }
+        int numConnected = 0;
+        for (int i = 0 ; i<n; i++){
+            if (ds.parent[i]==i) numConnected++;
+        }
+        return numConnected;
+    }
+};
